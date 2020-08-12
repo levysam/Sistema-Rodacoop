2e9e7efd6a5e148c226463805cd62ace3ee158f3
Levy <levyaraujo42@gmail.com>
2020-08-11 20:58:10 -0400

    Motorista model created

--- src/app/models/Motorista.js
+++ src/app/models/Motorista.js
@@ -1,37 +1,30 @@
 import Sequelize, { Model } from 'sequelize';
-import bcrypt from 'bcryptjs';
 
 class User extends Model {
 
   static init(sequelize) {
     super.init(
       {
-        id_user: {
+        id_motorista: {
           type: Sequelize.INTEGER,
           primaryKey: true,
-          autoIncrement: true
         },
-        email: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
-        password_hash: Sequelize.STRING,
-        acesso: Sequelize.CHAR,
+        id_pessoa: Sequelize.INTEGER,
+        id_centro_distribuicao: Sequelize.INTEGER,
+        id_forma_contratacao: Sequelize.INTEGER
       },
       {
         sequelize,
       }
     );
 
-    this.addHook('beforeSave', async user => {
-      if (user.password) {
-        user.password_hash = await bcrypt.hash(user.password, 8);
-      }
-    });
-
     return this;
   }
 
   static associate(models) {
-    this.belongsTo(models.Pessoa, {foreignKey: 'id_pessoa', as: 'pessoa'})
+    this.belongsTo(models.Pessoa, {foreignKey: 'id_pessoa', as: 'pessoa'});
+    this.belongsTo(models.centro_distribuicao, {foreignKey: 'id_centro_distribuicao', as: 'centroDistribuição'});
+    this.belongsTo(models.forma_contratacao, {foreignKey: 'id_forma_contratacao', as: 'formaContratacao'})
   }
 
   checkPassword(password) {
