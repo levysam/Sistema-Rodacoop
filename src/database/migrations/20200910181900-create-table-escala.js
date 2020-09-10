ccb22c1fda531e152368b3e71afd511b0333ce20
Levy <levyaraujo42@gmail.com>
2020-09-10 14:42:56 -0400

    create table escalas

---
+++ src/database/migrations/20200910181900-create-table-escala.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('escalas', {
+      id_escala: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+      },
+      id_motorista :{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "motoristas",
+          key: "id_motorista"
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      id_regiao_cluster:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "regiao_clusters",
+          key: "id_regiao_cluster"
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL'
+      },
+      situacao: {
+        type: Sequelize.STRING(1)
+      },
+      justificativa_desistencia: {
+        type: Sequelize.TEXT
+      },
+      data: {
+        type: Sequelize.DATE
+      },
+      id_user:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id_user"
+      },
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
